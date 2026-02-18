d61e534fa5271333b87f0f08b02c49ad0b7d509a
Syed-Dadapeer226 <syeddadapeer226@gmail.com>
2026-02-18 18:14:15 +0530

    feat: initial commit - end-to-end DevSecOps Tetris project on AWS EKS

---
+++ Jenkins-Server-TF/tools-install.sh
@@ -0,0 +1,86 @@
+# !/bin/bash
+
+# Author: Syed Dadapeer
+# Purpose: This script is used to install Java, Jenkins, Docker, Terraform, Kubectl, AWS CLI v2 and Trivy on the Jenkins server.
+# This script is tested on Ubuntu 24.04, you can modify the script according to your OS.
+# This script will also add the Jenkins user to the Docker group, so that Jenkins can run Docker commands.
+# This script will also run a Docker container of Sonarqube, so that you can use Sonarqube for code analysis.
+# Note: After running this script, you need to restart the Jenkins service to apply the changes.
+# You can run this script by using the following command:
+# chmod +x tools-install.sh
+# ./tools-install.sh
+
+# Update the package list
+sudo apt update
+
+# Intsalling Java
+sudo apt install fontconfig openjdk-21-jre -y
+java --version
+
+# Installing Jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
+jenkins_version=$(sudo systemctl status jenkins | grep -oP 'version \K[0-9.]+')
+echo "Jenkins version: $jenkins_version"
+
+# Installing Docker
+sudo apt update
+sudo apt install docker.io -y
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
+sudo chmod 777 /var/run/docker.sock
+docker --version
+
+# If you don't want to install Jenkins, you can create a container of Jenkins
+# docker run -d -p 8080:8080 -p 50000:50000 --name jenkins-container jenkins/jenkins:lts
+
+# Run Docker Container of Sonarqube
+docker run -d --name sonarqube -p 9000:9000 sonarqube:community
+docker ps -a
+
+
+# Installing Terraform
+sudo apt install unzip -y
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform -y
+terraform -version
+
+# Installing Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+# Installing AWS CLI v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+
+# Installing Trivy
+sudo apt-get install wget gnupg
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
+trivy --version
---
+++ README.md
@@ -0,0 +1,22 @@
+# 🎮 End-to-End-Kubernetes-DevSecOps-Tetris-Project
+---
+
+<p align="center">
+ <img src="https://img.shields.io/badge/AWS-EKS-orange?logo=amazonaws" />
+ <img src="https://img.shields.io/badge/Kubernetes-Ready-blue?logo=kubernetes" />
+ <img src="https://img.shields.io/badge/IaC-Terraform-purple?logo=terraform" />
+ <img src="https://img.shields.io/badge/CI-Jenkins-red?logo=jenkins" />
+ <img src="https://img.shields.io/badge/GitOps-ArgoCD-blue" />
+ <img src="https://img.shields.io/badge/Container-Docker-blue?logo=docker" />
+ <img src="https://img.shields.io/badge/Security-Trivy-green" />
+ <img src="https://img.shields.io/badge/Security-OWASP-red" />
+ <img src="https://img.shields.io/badge/CodeQuality-SonarQube-blue?logo=sonarqube" />
+ <img src="https://img.shields.io/badge/License-Apache2.0-green" />
+ <img src="https://img.shields.io/badge/Maintained-Yes-success" />
+ <img src="https://img.shields.io/badge/PRs-Welcome-brightgreen" />
+</p>
+
+---
+Hands-on DevSecOps project deploying a Tetris game on AWS EKS. Features Jenkins CI/CD, ArgoCD GitOps, Terraform-based IaC, Kubernetes, Docker, Trivy vulnerability scanning, OWASP Dependency-Check, and SonarQube for code quality.
+
+<p align="center"> <img width="1000" height="800" alt="image" src="https://github.com/user-attachments/assets/68972a69-10c0-4c00-89db-4841820328d1" /> </p>
